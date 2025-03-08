a9818e7675ddaf7ee3df8afd55ec2f8a7d158096
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2025-03-08 10:47:24 +0100

    Added a very simple pii detection agent with Ollama

--- ai/detect.ts
+++ ai/detect.ts
@@ -1,17 +1,18 @@
-"use server";
+// "use server";
 
 import { Redaction } from "./schemas";
+import { callPiiAgent } from "./agent";
 import { mockDetectPII } from "./mock";
 
 export async function detectPII(
   input: string,
   risk_tolerance: number
 ): Promise<Redaction[]> {
-  if (risk_tolerance < 0 || risk_tolerance > 1) {
-    throw new Error("Risk tolerance must be between 0 and 1");
-  }
+  const bounded_risk_tolerance = Math.min(Math.max(risk_tolerance, 0), 1);
 
-  // TODO: implement the anonymization logic
-  console.log("Anonymizing", input, risk_tolerance);
-  return mockDetectPII(input);
+  // const redactions = await callPiiAgent(input);
+  const redactions = mockDetectPII(input);
+  return redactions.filter(
+    (redaction) => redaction.confidence >= bounded_risk_tolerance
+  );
 }
