0103a9b333b10ea45cf6fa4d3966333130b8b4e2
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2025-03-13 15:24:10 +0100

    Fixing cache

--- ai/bartAgent.ts
+++ ai/bartAgent.ts
@@ -1,9 +1,10 @@
 import { Redaction } from "./schemas";
-import { LOAD_AND_REDACT_STORAGE } from "@/app/actions/load_and_redact";
+import { getCache } from "@/app/actions/load_and_redact";
 
 export async function callPiiAgent(): Promise<Redaction[]> {
     console.debug("Fetching BART redactions");
-    const response = LOAD_AND_REDACT_STORAGE[LOAD_AND_REDACT_STORAGE.length - 1].redactions;
+    const cache = await getCache();
+    const response = cache[cache.length - 1].redactions;
     console.debug("BART response:", response);
     return response;
 }
